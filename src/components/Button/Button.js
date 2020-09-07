2e2daeeafe6bdbaff666626997fe5f80af5a4b88
weihongfu <weihongfu@CrownNight.local>
2020-09-07 09:11:25 +0800

    添加节流方法

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -13,8 +13,19 @@ export default function CoustomButton(props) {
         props.click && props.click()
     }
 
+    function throttle(fn, delay) {
+        let last = new Date().valueOf();
+        return (...args) => {
+            let now = new Date().valueOf();
+            if(now - last >= delay) {
+                fn.apply(this, ...args);
+                last = now;
+            }
+        }
+    }
+
     const { name } = props;
-    return <Button onClick={_click}>{name}</Button>
+    return <Button onClick={throttle(_click, 3000)}>{name}</Button>
 }
 
 CoustomButton.propTypes = {
