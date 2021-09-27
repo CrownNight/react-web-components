d3967967107562d5c1f7840ba1d86f132cd53dfc
CrownNight <32128133+CrownNight@users.noreply.github.com>
2021-09-27 17:26:32 +0800

    格式化

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -8,37 +8,36 @@ import { Button } from 'antd';
  * @param {object} props the react props
  */
 export default function CoustomButton(props) {
+  function _click() {
+    props.click && props.click();
+  }
 
-    function _click() {
-        props.click && props.click()
-    }
+  function throttle(fn, delay) {
+    let last = new Date().valueOf();
+    return (...args) => {
+      let now = new Date().valueOf();
+      if (now - last >= delay) {
+        fn.apply(this, ...args);
+        last = now;
+      }
+    };
+  }
 
-    function throttle(fn, delay) {
-        let last = new Date().valueOf();
-        return (...args) => {
-            let now = new Date().valueOf();
-            if(now - last >= delay) {
-                fn.apply(this, ...args);
-                last = now;
-            }
-        }
-    }
-
-    const { name } = props;
-    return <Button onClick={throttle(_click, 3000)}>{name}</Button>
+  const { name } = props;
+  return <Button onClick={throttle(_click, 3000)}>{name}</Button>;
 }
 
 CoustomButton.propTypes = {
-    /** 
-     * 名称
-    */
-    name: PropTypes.string || PropTypes.element,
-    /** 
-     * 点击事件
-    */
-   click: PropTypes.func
-}
+  /**
+   * 名称
+   */
+  name: PropTypes.string || PropTypes.element,
+  /**
+   * 点击事件
+   */
+  click: PropTypes.func,
+};
 CoustomButton.defaultProps = {
-    name: "测试",
-    click: undefined
-}
+  name: '测试',
+  click: undefined,
+};
--- src/components/CommonSelect/Select.js
+++ src/components/CommonSelect/Select.js
@@ -9,66 +9,74 @@ const Option = Select.Option;
  * @param {object} props the props paramter
  */
 export default function CommonSelect(props) {
-    const {
-        dataSource,
-        isShowFirstCustomOption,
-        isShowEndCustomOption,
-        firstOptionName,
-        firstOptionValue,
-        endOptionName,
-        endOptionValue,
-        ...otherProps
-     } = props;
-    return <Select {...otherProps}>
-        {
-            isShowFirstCustomOption && <Option key={firstOptionValue} value={firstOptionValue}>{firstOptionName}</Option>
-        }
-        {
-            dataSource.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)
-        }
-        {
-            isShowEndCustomOption && <Option key={endOptionValue} value={endOptionValue}>{endOptionName}</Option>
-        }
+  const {
+    dataSource,
+    isShowFirstCustomOption,
+    isShowEndCustomOption,
+    firstOptionName,
+    firstOptionValue,
+    endOptionName,
+    endOptionValue,
+    ...otherProps
+  } = props;
+  return (
+    <Select {...otherProps}>
+      {isShowFirstCustomOption && (
+        <Option key={firstOptionValue} value={firstOptionValue}>
+          {firstOptionName}
+        </Option>
+      )}
+      {dataSource.map((item) => (
+        <Option key={item.id} value={item.id}>
+          {item.name}
+        </Option>
+      ))}
+      {isShowEndCustomOption && (
+        <Option key={endOptionValue} value={endOptionValue}>
+          {endOptionName}
+        </Option>
+      )}
     </Select>
+  );
 }
 
 CommonSelect.propTypes = {
-    /**
-     * 数据源，必须包含id和name字段
-     */
-    dataSource: PropTypes.array.isRequired,
-    /**
-     * 是否展示自定义第一条option数据
-     */
-    isShowFirstCustomOption: PropTypes.bool,
-    /**
-     * 是否展示自定义最后一条option数据
-     */
-    isShowEndCustomOption: PropTypes.bool,
-    /**
-     * 自定义第一条option数据的value值
-     */
-    firstOptionValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    /**
-     * 自定义第一条option数据的name值
-     */
-    firstOptionName: PropTypes.any,
-    /**
-     * 自定义最后一条option数据的value值
-     */
-    endOptionValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    /**
-     * 自定义最后一条option数据的name值
-     */
-    endOptionName: PropTypes.any
+  /**
+   * 数据源，必须包含id和name字段
+   */
+  dataSource: PropTypes.array.isRequired,
+  /**
+   * 是否展示自定义第一条option数据
+   */
+  isShowFirstCustomOption: PropTypes.bool,
+  /**
+   * 是否展示自定义最后一条option数据
+   */
+  isShowEndCustomOption: PropTypes.bool,
+  /**
+   * 自定义第一条option数据的value值
+   */
+  firstOptionValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /**
+   * 自定义第一条option数据的name值
+   */
+  firstOptionName: PropTypes.any,
+  /**
+   * 自定义最后一条option数据的value值
+   */
+  endOptionValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /**
+   * 自定义最后一条option数据的name值
+   */
+  endOptionName: PropTypes.any,
 };
 
 CommonSelect.defaultProps = {
-    dataSource: [],
-    isShowEndCustomOption: false,
-    isShowFirstCustomOption: false,
-    firstOptionName: '',
-    firstOptionValue: '',
-    endOptionName: '',
-    endOptionValue: ''
-}
+  dataSource: [],
+  isShowEndCustomOption: false,
+  isShowFirstCustomOption: false,
+  firstOptionName: '',
+  firstOptionValue: '',
+  endOptionName: '',
+  endOptionValue: '',
+};
